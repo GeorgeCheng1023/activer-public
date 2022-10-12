d6e4e99cb5b7799258cf240485f2af18d40bcb0c
DACHXY <danny10132024@gmail.com>
2022-10-12 22:07:30 +0800

    style: adjust header style and add logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import { Link } from 'react-router-dom';
 import './index.scss';
+
+// component
+import Navbar from './components/Navbar';
 
 // icon
 import { IconLogoPrimary } from '../Icons';
@@ -8,7 +11,9 @@ import { IconLogoPrimary } from '../Icons';
 function Header() {
   return (
     <div className="header">
-      <IconLogoPrimary />
+      <Link className="logo" to="/home">
+        <IconLogoPrimary />
+      </Link>
       <Navbar />
     </div>
   );
