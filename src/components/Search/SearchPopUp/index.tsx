6b2110dee4ff630d939758918cb55c98fa91f60e
George Cheng <ck40292@gmail.com>
2022-10-13 01:11:22 +0800

    feat: init search

---
+++ src/components/Search/SearchPopUp/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+
+function Search() {
+  return (
+    <div className="search">
+      <h1>Search</h1>
+      <p>This is init Search</p>
+    </div>
+  );
+}
+
+export default Search;
---
+++ src/pages/Search.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchPopUp from '../components/Search/SearchPopUp';
+
+function Search() {
+  return (
+    <>
+      <h1>SearchPopup</h1>
+      <p> This is Search Popup </p>
+      <SearchPopUp />
+    </>
+  );
+}
+
+export default Search;
