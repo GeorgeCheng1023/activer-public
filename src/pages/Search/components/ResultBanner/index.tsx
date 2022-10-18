48ec0b8f2d428a88d034a7680af7b4d628a12ca0
George Cheng <ck40292@gmail.com>
2022-10-18 10:57:00 +0800

    feat: search result banner

--- src/pages/Search/components/ResultBanner/index.tsx
+++ src/pages/Search/components/ResultBanner/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import './index.scss';
 import Button from '../../../../components/Button';
 
 function ResultBanner() {
   return (
-    <>
-      <h1>Search Result</h1>
-      <Button color="primary" decoration="default" text="edit" />
-    </>
+    <div className="result__banner">
+      <h1 className="result__banner__h1 ">搜尋結果</h1>
+      <div className="result__banner__btn">
+        <Button color="primary" decoration="outline" text="修改搜尋條件" />
+      </div>
+    </div>
   );
 }
+
+export default ResultBanner;
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 // import SearchPopUp from './components/SearchPopUp';
 import { CardColumn } from '../../components/Card';
+import ResultBanner from './components/ResultBanner';
 
 function Search() {
   return (
     <>
-      <h1>SearchPopup</h1>
-      <p> This is Search Popup </p>
+      <ResultBanner />
       <CardColumn
         imgUrl="https://plus.unsplash.com/premium_photo-1661329835271-c130b1ea3f28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60
       "
