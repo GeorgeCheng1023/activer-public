cbd0307b6f9ecb5a3c5de31583b43c18758abb13
George Cheng <ck40292@gmail.com>
2022-10-19 18:39:13 +0800

    feat: update router to v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,61 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  RouterProvider, createBrowserRouter,
+} from 'react-router-dom';
 
 // components
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Search from './pages/Search';
+import User, {
+  Basic, Account, History, Preferences, Manage,
+} from './pages/User';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/search',
+    element: <Search />,
+  },
+  {
+    path: '/user',
+    element: <User />,
+    children: [
+      {
+        path: 'basic',
+        element: <Basic />,
+      },
+      {
+        path: 'account',
+        element: <Account />,
+      },
+      {
+        path: 'manage',
+        element: <Manage />,
+      },
+      {
+        path: 'history',
+        element: <History />,
+      },
+      {
+        path: 'preferences',
+        element: <Preferences />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <RouterProvider router={router} />
   );
 }
 
