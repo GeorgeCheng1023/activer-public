7c24d418460f2e6847dc150aa92b06f0bc928712
George Cheng <ck40292@gmail.com>
2022-10-20 14:31:41 +0800

    feat: Root page router

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 // component
-import Header from '../../components/Header';
 import Hero from './components/Hero';
 import Trend from './components/Trend';
 
 function Home() {
   return (
     <div className="home">
-      <Header />
       <Hero />
       <Trend />
     </div>
