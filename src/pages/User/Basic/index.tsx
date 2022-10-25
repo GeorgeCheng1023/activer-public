84c48d613c496750c7d4f6e6befa8a15ef56fc57
George Cheng <ck40292@gmail.com>
2022-10-25 13:50:30 +0800

    feat: user-manage nav

--- src/pages/User/Basic/index.tsx
+++ src/pages/User/Basic/index.tsx
@@ -7,7 +7,7 @@ function Basic() {
     <div className="user-basic user-basic__left">
       <div className="user-basic__img">
         <img
-          src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+          src="https://images.unsplash.com/photo-1561495376-dc9c7c5b8726?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9nfGVufDB8MnwwfHw%3D&auto=format&fit=crop&w=500&q=60"
           alt="userHead"
         />
       </div>
--- src/pages/User/Manage/index.tsx
+++ src/pages/User/Manage/index.tsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import { BiBorderAll } from 'react-icons/bi';
+import { BsBookmarkHeart } from 'react-icons/bs';
+import { AiOutlineCheckCircle } from 'react-icons/ai';
+import ManageNav from './components/ManageNav';
 
 function Manage() {
+  const changeFilterHandler = (id: number) => {
+    console.log(id);
+  };
+
   return (
-    <h1>This is Manage Page</h1>
+    <>
+      <h1>This is Manage Page</h1>
+      <ManageNav
+        // onClick={clickHandler}
+        buttons={
+          [
+            {
+              title: '全部',
+              icon: <BiBorderAll />,
+            },
+            {
+              title: '已報名',
+              icon: <AiOutlineCheckCircle />,
+            },
+            {
+              title: '願望',
+              icon: <BsBookmarkHeart />,
+            },
+          ]
+        }
+        onChangeFilter={changeFilterHandler}
+      />
+    </>
   );
 }
 
