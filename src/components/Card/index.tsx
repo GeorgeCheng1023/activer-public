f5a224071aea35d4e43f49d4bc7bca5b33274777
George Cheng <ck40292@gmail.com>
2022-10-25 16:03:46 +0800

    feat: Card Column props

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -26,23 +26,37 @@ function Card({
   );
 }
 
-export function CardColumn({
-  imgUrl, title, tags, animation, altText, detail,
-}: CardType) {
+type CardColumnProps = {
+  data: CardType;
+  control?: JSX.Element;
+};
+
+export function CardColumn({ data, control }: CardColumnProps) {
+  const {
+    imgUrl, altText, title, tags, detail,
+    animation,
+  } = data;
   return (
     <div className={`card-column card--${animation}`}>
       <img className="card-column__image" src={imgUrl} alt={altText} />
       <div className="card-column__content">
         <h3 className="card-column__title">{title}</h3>
-        <div className="card-co lumn__tag">
+        <div className="card-column__tag">
           {tags.map(createTag)}
         </div>
         <div className="card-column__detail">
           {detail}
         </div>
       </div>
+      <div className="card-column__control">
+        {control}
+      </div>
     </div>
   );
 }
+
+CardColumn.defaultProps = {
+  control: null,
+};
 
 export default Card;
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { CardColumn } from '../../components/Card';
 import ResultBanner from './components/ResultBanner';
 
+const data = {
+  imgUrl: 'https://plus.unsplash.com/premium_photo-1661329835271-c130b1ea3f28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+  title: 'img',
+  altText: 'img',
+  tags: [{ color: 'primary', text: 'tag' }],
+  animation: '',
+  detail: 'Date: 2022/10/2 ~ 2022/10/3',
+};
+
 function Search() {
   return (
     <>
       <ResultBanner />
       <CardColumn
-        imgUrl="https://plus.unsplash.com/premium_photo-1661329835271-c130b1ea3f28?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60
-      "
-        title="img"
-        altText="img"
-        tags={[{ color: 'primary', text: 'tag' }]}
-        animation=""
-        detail="Date: 2022/10/2 ~ 2022/10/3"
+        data={data as CardType}
       />
     </>
 
