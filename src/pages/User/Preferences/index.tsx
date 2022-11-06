30b511824ca2930a6db10f1a49cbad2c644df692
George Cheng <ck40292@gmail.com>
2022-11-06 21:11:30 +0800

    feat: init preference tag sort

--- src/pages/User/Preferences/index.tsx
+++ src/pages/User/Preferences/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import TagSort from 'components/TagSort';
+// import FormSearchBar from 'components/Form/FormSearchBar';
+import dummySearchHistory from './dummySearchHistory.json';
 
 function Preferences() {
   return (
     <>
       <h2>您的預設標籤庫</h2>
       <h2>搜尋紀錄</h2>
-      <TagSort />
-      <div />
-
+      {
+        dummySearchHistory.map((history: any) => (
+          <TagSort defaultTags={history.HistoryTags} />
+        ))
+      }
     </>
   );
 }
