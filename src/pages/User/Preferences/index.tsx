949e94f379fe72a377a4931d8f973e76e8129ac0
George Cheng <ck40292@gmail.com>
2022-11-18 15:10:15 +0800

    feat : preferences defaultTag

--- src/pages/User/Preferences/index.tsx
+++ src/pages/User/Preferences/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// components
 import TagSort from 'components/TagSort';
 import SearchBar from 'components/Form/FormSearchBar';
 import Button from 'components/Button';
 import { BiSend } from 'react-icons/bi';
+import Tag, { TagType } from 'components/Tag';
+
+// data
+import dummyUserDefaultTags from './dummyUserDefaultTags.json';
 import dummySearchHistory from './dummySearchHistory.json';
+
+// style
 import './index.scss';
 
 // parseData
@@ -18,15 +26,56 @@ const parseDummySearchHistory = dummySearchHistory.map((history) => ({
   )),
 }));
 
+const parseDefaultTags = dummyUserDefaultTags.DefaultTags.map((tag: any) => ({
+  id: tag.TagId,
+  text: tag.Text,
+  variant: tag.Type,
+}));
+
 const handleSubmit = (input: string) => {
   console.log(input);
 };
 
 function Preferences() {
+  // eslint-disable-next-line
+  const [defaultTags, setDefaultTags] = useState(parseDefaultTags);
+
+  const handleSearchTagSubmit = (inputValue : string) => {
+    // eslint-disable-next-line
+    console.log(inputValue)
+  };
+
+  const handleSaveSubmit:
+  React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    // eslint-disable-next-line
+    console.log(defaultTags);
+  };
+
   return (
     <div className="preferences">
-      <h2 className="preferences__h2">您的預設標籤庫</h2>
+      <div className="preferences__default-tags">
+        <div className="preferences__default-tags__control">
+          <h2 className="preferences__h2">您的預設標籤庫</h2>
+          <div className="preferences__default-tags__control__right">
+            <SearchBar placeHolder="搜尋標籤" onSubmit={handleSearchTagSubmit} />
+            <Button buttonType="submit" onClick={handleSaveSubmit} text="儲存" />
+            <Button buttonType="submit" onClick={handleSaveSubmit} text="取消" variant="outline" />
+          </div>
+        </div>
+        <div className="preferences__default-tags__container">
+          {defaultTags.map((tag: TagType) => (
+            <Tag
+              key={tag.id}
+              id={tag.id.toString()}
+              variant={tag.variant}
+              text={tag.text}
+              icon="minus"
+            />
+          ))}
 
+        </div>
+      </div>
       <h2 className="preferences__h2">搜尋紀錄</h2>
       <div className="preferences__history">
         {
