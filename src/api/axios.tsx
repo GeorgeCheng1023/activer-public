79976309975ee8ac6d63bfcdea6e20a57bfe1eb2
onandon <Your email>
2022-12-03 14:26:15 +0800

    fix: axios

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -51,3 +51,15 @@ export const apiUserGoogleData = (access_token: string) => axios.get(
     },
   },
 );
+
+// activity api
+const activityRequest = axios.create({
+  baseURL: TEST_URL.concat('/api/activity'),
+});
+export const getActivity = (id: string) => activityRequest.get(`/${id}`, {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosTest;
