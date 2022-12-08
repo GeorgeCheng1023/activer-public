23cf84d2a3cc4ec966e66346f59061798074855e
GeorgeCheng(Laptop) <ck40292@gmail.com>
2022-12-08 11:03:51 +0800

    feat: portrait file type to base64

--- src/components/Crop/utils/cropImages.tsx
+++ src/components/Crop/utils/cropImages.tsx
@@ -80,16 +80,16 @@ const getCroppedImg = async (
   ctx.putImageData(data, 0, 0);
 
   // As Base64 string
-  // return canvas.toDataURL('image/jpeg');
+  return canvas.toDataURL('image/jpeg');
 
-  // As a blob
-  return new Promise((resolve) => {
-    canvas.toBlob((file) => {
-      if (file) {
-        resolve(URL.createObjectURL(file));
-      }
-    }, 'image/jpeg');
-  });
+  // As a <blob></blob>
+  // return new Promise((resolve) => {
+  //   canvas.toBlob((file) => {
+  //     if (file) {
+  //       resolve(URL.createObjectURL(file));
+  //     }
+  //   }, 'image/jpeg');
+  // });
 };
 
 export default getCroppedImg;
