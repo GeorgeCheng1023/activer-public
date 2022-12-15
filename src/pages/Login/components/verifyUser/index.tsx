b35f578d0e85fd5b9df241112d644d1d5b6c5979
onandon <047633597q@gmail.com>
2022-12-15 17:32:21 +0800

    feat: verify and password pages

--- src/pages/Login/components/verifyUser/index.tsx
+++ src/pages/Login/components/verifyUser/index.tsx
@@ -1,7 +1,104 @@
 import React from 'react';
+import './index.scss';
+
+// components
+import Button from 'components/Button';
+import { Link } from 'react-router-dom';
 
 function VerifyUser() {
-  return <div>verifyUser</div>;
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    const keyCode = e.key;
+    if (/[0-9]/.test(keyCode) || keyCode === 'Backspace') {
+      return e.key;
+    }
+  };
+
+  return (
+    <div className="verify-user__container">
+      <main className="verify-user">
+        <h1 className="verify-user__title">輸入驗證碼</h1>
+
+        <section className="verify-user__section">
+          <div className="verify-user__section__code">
+            <input
+              className="verify-user__section__number"
+              id="verify-code-1"
+              name="verify-code-1"
+              type="text"
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+            />
+          </div>
+
+          <div className="verify-user__section__code">
+            <input
+              className="verify-user__section__number"
+              id="verify-code-2"
+              name="verify-code-2"
+              type="text"
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+            />
+          </div>
+
+          <div className="verify-user__section__code">
+            <input
+              className="verify-user__section__number"
+              id="verify-code-3"
+              name="verify-code-3"
+              type="text"
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+            />
+          </div>
+
+          <div className="verify-user__section__code">
+            <input
+              className="verify-user__section__number"
+              id="verify-code-4"
+              name="verify-code-4"
+              type="text"
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+            />
+          </div>
+
+          <div className="verify-user__section__code">
+            <input
+              className="verify-user__section__number"
+              id="verify-code-5"
+              name="verify-code-5"
+              type="text"
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+            />
+          </div>
+
+          <div className="verify-user__section__code">
+            <input
+              className="verify-user__section__number"
+              id="verify-code-6"
+              name="verify-code-6"
+              type="text"
+              onKeyDown={handleKeyDown}
+              maxLength={1}
+            />
+          </div>
+        </section>
+
+        <section className="verify-user__resend">
+          <h5 className="verify-user__resend__text">未收到驗證碼?</h5>
+          <Link to="/password">
+            <h5 className="verify-user__resend__btn">
+              重新驗證
+            </h5>
+          </Link>
+        </section>
+
+        <Button color="secondary" text="寄出" />
+      </main>
+    </div>
+  );
 }
 
 export default VerifyUser;
