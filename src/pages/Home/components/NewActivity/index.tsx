46ae9a3d97a7cee7bedab170f9d9725df9c79d36
GeorgeCheng1023 <ck40292@gmail.com>
2023-01-08 12:09:41 +0800

    feat: hero animate

--- src/pages/Home/components/NewActivity/index.tsx
+++ src/pages/Home/components/NewActivity/index.tsx
@@ -7,6 +7,7 @@ import { Card } from 'components/Card';
 import useWindowWidth from 'hooks/window/useWindowWidth';
 import { FcPositiveDynamic } from 'react-icons/fc';
 import dummyData from './dummyTrendActivity.json';
+import './index.scss';
 
 function NewActivity() {
   const screenWidth = useWindowWidth();
