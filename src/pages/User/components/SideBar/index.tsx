a2505686cd06e98f06006a921709c45648a3950e
GeorgeCheng1023 <ck40292@gmail.com>
2023-01-14 13:48:40 +0800

    feat: User Sidebar style

--- src/pages/User/components/SideBar/index.tsx
+++ src/pages/User/components/SideBar/index.tsx
@@ -4,6 +4,7 @@ import {
 } from 'react-icons/bs';
 import classNames from 'classnames';
 import { BiUser, BiHistory } from 'react-icons/bi';
+import { IconLogo } from 'components/Icons';
 
 import SideBarLink from './components/SidebarLink';
 import './index.scss';
@@ -26,6 +27,10 @@ function SideBar() {
   return (
 
     <div className={sidebarClasses} ref={sideBarRef}>
+      <div className="sidebar__header">
+        <IconLogo />
+        使用者專區
+      </div>
       <SideBarLink text="基本資料" url="/user/basic" icon={<BiUser />} />
       <SideBarLink text="帳號安全" url="/user/account" icon={<BsFillShieldLockFill />} />
       <SideBarLink text="管理活動" url="/user/manage" icon={<BsGearFill />} />
--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -9,22 +9,18 @@ import History from './History';
 import Manage from './Manage';
 import Preferences from './Preferences';
 import SideBar from './components/SideBar';
-import Banner from './components/Banner';
 
 function User() {
   // eslint-disable-next-line
   const [currentText, setCurrentText] = useState('');
 
   return (
-    <>
-      <Banner text={currentText} />
-      <div className="user__container">
-        <SideBar />
-        <div className="user__main-content">
-          <Outlet />
-        </div>
+    <div className="user__container">
+      <SideBar />
+      <div className="user__main-content">
+        <Outlet />
       </div>
-    </>
+    </div>
   );
 }
 
