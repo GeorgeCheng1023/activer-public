99457f9f2c2bdb66d705201cee9529a73967405e
GeorgeCheng1023 <ck40292@gmail.com>
2023-01-14 23:29:22 +0800

    feat: Basic location faq tooltip

--- src/pages/User/Basic/index.tsx
+++ src/pages/User/Basic/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import './index.scss';
-import FAQTag from 'components/FAQ-Tag';
 import Crop from 'components/Crop';
 import { FormInputFile, FormInput, FormDropDown } from 'components/Form';
 import useNonInitialEffect from 'hooks/react/useNonInitialEffect';
 import Button from 'components/Button';
 import { getUserData, userUpdate } from 'store/userAuth';
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import { useAppDispatch, useAppSelector } from 'hooks/redux';
 import { apiUserUpdate } from 'api/axios';
+import { Tooltip } from 'react-tooltip';
 import CityCountyData from './CityCountyData.json';
 
 function Basic() {
@@ -61,134 +62,147 @@ function Basic() {
 
   return (
     <form onSubmit={handleSubmit} name="userFormData" className="user-basic">
-      <div className="user-basic__container">
-        <div className="user-basic__container--column">
-          <div className="user-basic__portrait">
-            <Crop
-              image={imageSrc}
-              onCropped={handleCropped}
-              onClose={() => setDisplayCropPanel(false)}
-              display={displayCropPanel}
-            />
-            <img className="user-basic__portrait img" src={values.Portrait || '/user.png'} alt="user-portrait" />
-            <div className="user-basic__portrait upload-button">
-              <FormInputFile
-                name="Portrait"
-                setImageSrc={setImageSrc}
-                accept="image/*"
-                id="user-basic__portrait__upload"
-                label="上傳頭像"
-              />
-            </div>
-          </div>
-          <div className="user-basic__input user-basic__input__nick-name">
-            <FormInput
-              id="nick_name"
-              name="NickName"
-              label="暱稱"
-              type="text"
-              placeholder="輸入暱稱姓名"
-              errorMessage="暱稱不可超過15字"
-              pattern="[\u4E00-\u9FFFA-Za-z]{1,15}"
-              formValue={values}
-              onChange={handleChange}
-            />
-          </div>
-        </div>
-        <div className="user-basic__container--column">
-          <div className="user-basic__input user-basic__input__real-name">
-            <FormInput
-              id="real_name"
-              name="RealName"
-              label="真實姓名"
-              type="text"
-              placeholder="輸入真實姓名"
-              errorMessage="真實只能接受2-6字"
-              pattern="[\u4E00-\u9FFF]{2,6}"
-              formValue={values}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="user-basic__container">
-            <div className="user-basic__input user-basic__input__gender">
-              <FormDropDown
-                id="gender"
-                label="性別"
-                name="Gender"
-                options={['男性', '女性', '其他', '隱藏']}
-                value={values.Gender}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="user-basic__input user-basic__input__birthday">
-              <FormInput
-                id="Birthday"
-                name="Birthday"
-                label="生日"
-                type="date"
-                placeholder="請選擇出生年月日"
-                formValue={values}
-                onChange={handleChange}
-              />
-            </div>
-          </div>
-          <div className="user-basic__input user-basic__input__profession">
-            <FormInput
-              id="Profession"
-              name="Profession"
-              label="職業"
-              type="text"
-              placeholder="請選擇輸入您的職業"
-              formValue={values}
-              onChange={handleChange}
-            />
-          </div>
-          <div className="user-basic__input user-basic__input__location">
-            <h3 className="user-basic__input__location__label">
-              居住地
-            </h3>
-            <div className="user-basic__container">
-              <div className="user-basic__input__location__county">
-                <FormDropDown
-                  id="country"
-                  label="縣市"
-                  name="County"
-                  options={CityCountyData.map((c) => c.CityName)}
-                  value={values.County}
-                  onChange={handleCountyChange}
-                />
-              </div>
-              <div className="user-basic__input__location__area">
-                <FormDropDown
-                  id="area"
-                  label="區鄉鎮"
-                  name="Area"
-                  options={CityCountyData.find(
-                    (c) => c.CityName === selectedCounty,
-                  )?.AreaList.map((a) => a.AreaName) || []}
-                  value={values.Area}
-                  onChange={handleChange}
-                />
-              </div>
-            </div>
-            <FAQTag title="為什麼需要提供居住地?" dataMsg="提供居住地可以讓我們協助搜索鄰近您居住地的活動!" />
-          </div>
 
-          <div className="user-basic__input user-basic__input__phone">
-            <FormInput
-              id="phone"
-              name="Phone"
-              label="電話"
-              type="text"
-              placeholder="請選擇輸入您的職業"
-              pattern="[0-9]{10}"
-              errorMessage="電話必須是10位數字"
-              formValue={values}
-              onChange={handleChange}
+      <div className="user-basic__container user-basic__basic">
+        <h2>基本資訊</h2>
+        {/* Portrait */}
+        <div className="user-basic__portrait">
+          <Crop
+            image={imageSrc}
+            onCropped={handleCropped}
+            onClose={() => setDisplayCropPanel(false)}
+            display={displayCropPanel}
+          />
+          <img className="user-basic__portrait img" src={values.Portrait || '/user.png'} alt="user-portrait" />
+          <div className="user-basic__portrait upload-button">
+            <FormInputFile
+              name="Portrait"
+              setImageSrc={setImageSrc}
+              accept="image/*"
+              id="user-basic__portrait__upload"
+              label="上傳頭像"
             />
           </div>
-          <Button type="submit" text="確認修改" />
         </div>
+        {/* Nickname */}
+        <div className="user-basic__input user-basic__input__nick-name">
+          <FormInput
+            id="nick_name"
+            name="NickName"
+            label="暱稱"
+            type="text"
+            placeholder="輸入暱稱姓名"
+            errorMessage="暱稱不可超過15字"
+            pattern="[\u4E00-\u9FFFA-Za-z]{1,15}"
+            formValue={values}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Realname */}
+        <div className="user-basic__input user-basic__input__real-name">
+          <FormInput
+            id="real_name"
+            name="RealName"
+            label="真實姓名"
+            type="text"
+            placeholder="輸入真實姓名"
+            errorMessage="真實只能接受2-6字"
+            pattern="[\u4E00-\u9FFF]{2,6}"
+            formValue={values}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Gender */}
+        <div className="user-basic__input user-basic__input__gender">
+          <FormDropDown
+            id="gender"
+            label="性別"
+            name="Gender"
+            options={['男性', '女性', '其他', '隱藏']}
+            value={values.Gender}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Birthday */}
+        <div className="user-basic__input user-basic__input__birthday">
+          <FormInput
+            id="Birthday"
+            name="Birthday"
+            label="生日"
+            type="date"
+            placeholder="請選擇出生年月日"
+            formValue={values}
+            onChange={handleChange}
+          />
+        </div>
+        {/* Profession */}
+        <div className="user-basic__input user-basic__input__profession">
+          <FormInput
+            id="Profession"
+            name="Profession"
+            label="職業"
+            type="text"
+            placeholder="請選擇輸入您的職業"
+            formValue={values}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="user-basic__container">
+        <h2>地址</h2>
+        {/* Location: County */}
+        <div className="user-basic__input user-basic__input__location__county">
+          <FormDropDown
+            id="country"
+            label="縣市"
+            name="County"
+            options={CityCountyData.map((c) => c.CityName)}
+            value={values.County}
+            onChange={handleCountyChange}
+          />
+        </div>
+        {/* Location: Area */}
+        <div className="user-basic__input user-basic__input__location__area">
+          <FormDropDown
+            id="area"
+            label="區鄉鎮"
+            name="Area"
+            options={CityCountyData.find(
+              (c) => c.CityName === selectedCounty,
+            )?.AreaList.map((a) => a.AreaName) || []}
+            value={values.Area}
+            onChange={handleChange}
+          />
+        </div>
+        <Button text="為什麼需要提供居住地?" color="white" iconBefore={<AiOutlineQuestionCircle />} id="user-basic__location-faq" />
+        <Tooltip
+          anchorId="user-basic__location-faq"
+          content="提供居住地可以讓我們協助搜索鄰近您居住地的活動!"
+        />
+      </div>
+
+      <div className="user-basic__container">
+        <h2>聯絡資訊</h2>
+
+        {/* Phone */}
+        <div className="user-basic__input user-basic__input__phone">
+          <FormInput
+            id="phone"
+            name="Phone"
+            label="電話"
+            type="text"
+            placeholder="請選擇輸入您的職業"
+            pattern="[0-9]{10}"
+            errorMessage="電話必須是10位數字"
+            formValue={values}
+            onChange={handleChange}
+          />
+        </div>
+      </div>
+
+      <div className="user-basic__submit">
+        <Button type="submit" text="確認修改" />
       </div>
     </form>
   );
