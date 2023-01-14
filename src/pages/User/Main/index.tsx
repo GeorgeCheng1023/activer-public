2bb9b9e3f6cfa278ff88e7e781838191d5f0121c
GeorgeCheng1023 <ck40292@gmail.com>
2023-01-14 16:31:24 +0800

    feat: User Main

--- src/pages/User/Main/index.tsx
+++ src/pages/User/Main/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { getUserNickname } from 'store/userAuth';
 import { useAppSelector } from 'hooks/redux';
+import './index.scss';
 
 function Main() {
   const nickname = useAppSelector(getUserNickname);
